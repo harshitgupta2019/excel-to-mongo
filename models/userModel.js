b0f62f4074998019545442326da50309cb3e783e
Harshit <hargup3438@gmail.com>
2022-09-27 20:42:56 +0530

    make it better

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+var mongoose  =  require('mongoose');  
+   
+var excelSchema = new mongoose.Schema({
+    name:String,
+    // class:Number,
+    // subject:String
+    email:String,
+    mobile:String,
+    dob:Date,
+    experience:String,
+    resume:String,
+    location:String,
+    address:String,
+    employer:String,
+    designation:String
+});
+
+
+module.exports = mongoose.model('excelData',excelSchema); 
